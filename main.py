f2b8c80648f5191debf9f03c9dc5cfc053062cb9
Pedro Costa <pedrocosta2802@gmail.com>
2025-08-27 16:17:17 -0300

    Add saving as pdf method

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import yt_dlp
 import whisper
 from transformers import AutoTokenizer, AutoModelForCausalLM
+from fpdf import FPDF
 import torch
 import os
+from datetime import datetime
+
 
 #get yt audio 
 def audio_download(url, output_path='audio'):
@@ -30,13 +33,35 @@ def audio_transcribe(audio_path, model_name='small'):
 #summarize in topics (Llama)
 def text_summa(text, model_path='./models/llama-2-7b-chat.Q4_0.gguf'):
     from llama_cpp import Llama
-    model = Llama(model_path, n_ctx=2048) #context size
+    model = Llama(model_path, n_ctx=4096) #context size
 
-    prompt = f"Please resume the next text in principal topics(bullet points), get me the key dots: {text[:4000]}"
+    prompt = f"Please resume the next text in principal topics(bullet points), get me the key dots: {text[:6000]}"
 
-    response = model(prompt, max_tokens=500, temperature=0.5)
+    response = model(prompt, max_tokens=1000, temperature=0.5)
     summary = response['choices'][0]['text']
     return summary
+
+#save the summary at a pdf
+def save_pdf(summary, pdf_path='resumo_video.pdf'):
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font("Arial", size=12)
+    
+    #tittle
+    pdf.cell(200, 10, txt="Summary from the YT video", ln=1, align='C')
+
+    #Date
+    current_date = datetime.now().strftime("%d/%m/%Y %H:%M")
+    pdf.cell(200, 10, txt=f"Gerado em {current_date}", ln=1, align='L')
+
+    #Content
+    pdf.ln(10)
+    for line in summary.split('\n'):
+        pdf.multi_cell(0, 10, txt=line)
+    
+    pdf.output(pdf_path)
+    print("PDF saved")
+
 
 #main
 if __name__ == "__main__":
@@ -44,5 +69,4 @@ if __name__ == "__main__":
     audio_file = audio_download(video_url)
     transcription = audio_transcribe(audio_file)
     summary = text_summa(transcription)
-    print("\nTopics:\n")
-    print(summary)
+    save_pdf(summary)
